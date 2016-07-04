447e4bdf0941fa484732108311f717510277d50c
tangzhentao <tangzhentaoxl@sina.cn>
2016-07-04 22:11:26 +0800

    add initial project.

---
+++ UsingCocoaPods/UsingCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UsingCocoaPods
+//
+//  Created by tang on 16/7/4.
+//  Copyright © 2016年 tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UsingCocoaPods/UsingCocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UsingCocoaPods
+//
+//  Created by tang on 16/7/4.
+//  Copyright © 2016年 tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
